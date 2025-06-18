66f1cbac8b988780fc9010090544f85d70dcd8de
Nihad <43179082+N-ihad@users.noreply.github.com>
2025-06-18 16:02:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
   ],
   dependencies: [
     .package(
-      url: "https://github.com/googlemaps/ios-maps-sdk",
-      from: "9.4.0"),
+      url: "https://github.com/N-ihad/ios-maps-sdk",
+      revision: "f90484603ae062ed082b5f58b5ca0ad6fadcb102"),
     .package(
       url: "https://github.com/erikdoe/ocmock.git",
       revision: "fe1661a3efed11831a6452f4b1a0c5e6ddc08c3d"),
